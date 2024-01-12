73174908f841bc7fe0ab6cc2fed2a4b62de51ae4
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-01-12 02:10:50 -0400

    fix emails

--- backend/resources/views/emails/layouts2/header.blade.php
+++
@@ -1,4 +0,0 @@
-<a  target="_blank" href="{{ env('APP_DOMAIN') }}" style="-webkit-text-size-adjust:none;text-decoration:underline;color:#2CB543;font-size:14px">
-    <img src="{{ asset('/logos/slogan.png') }}" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" width="300">
-</a>
-                
