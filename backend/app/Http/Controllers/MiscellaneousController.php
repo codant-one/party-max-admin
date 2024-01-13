432ce97b07251ee4c5a97d0575a393303e76601f
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-01-13 00:53:50 -0400

    fix categorie slug

--- backend/app/Http/Controllers/MiscellaneousController.php
+++ backend/app/Http/Controllers/MiscellaneousController.php
@@ -23,11 +23,11 @@ class MiscellaneousController extends Controller
                                 ->where('slug', $slug)
                                 ->first();
     
-            $products = Product::with(['user'])
-                               ->join('product_colors', 'products.id', '=', 'product_colors.product_id')
-                               ->join('product_categories', 'product_colors.id', '=', 'product_categories.product_color_id')
-                               ->where('product_categories.category_id', $category->id)
-                               ->orderBy('products.created_at', 'desc')
+            $products = Product::with(['user', 'colors.categories'])
+                                ->whereHas('colors.categories', function($query) use ($category) {
+                                    $query->where('category_id', $category->id);
+                                })
+                               ->orderBy('created_at', 'desc')
                                ->limit(5)
                                ->get();
             
