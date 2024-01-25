a39d0a44444cc1d08e84092ddd713bf10c10843a
Diego Bolivar <Dbolivarv90@gmail.com>
2024-01-24 22:36:37 -0500

    add CarController

--- backend/app/Http/Controllers/Shopping/CartController.php
+++ backend/app/Http/Controllers/Shopping/CartController.php
@@ -19,12 +19,29 @@ class CartController extends Controller
        
         try 
         {
-            $cart = ShoppingCart::with(['client','product'])->where('client_id',$id)->get();
+            $cart = ShoppingCart::with(['client','product.user'])
+                                        ->where('client_id',$id)
+                                        ->get()
+                                        ->groupBy('client_id') // Agrupa por client_id
+                                        ->map(function ($group) {
+                                            // La función de mapeo para agregar detalles del producto y cantidad a cada elemento del grupo
+                                            return [
+                                                'cliente' => $group->first()->client,
+                                                'detalles' => $group->map(function ($item) {
+
+                                                    $producto = $item->product;
+                                                    $producto->quantity = $item->quantity;
+                                                    return $producto;
+                                                })->all(),
+                                            ];
+                                        })
+                                        ->values()
+                                        ->all();
 
             return response()->json([
                 'success' => true,
                 'data' => [ 
-                    'cart' => $cart
+                    'cart' => $cart[0]
                 ]
             ], 200);
         } catch (\Illuminate\Database\QueryException $ex) {
