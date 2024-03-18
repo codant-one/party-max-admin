5fce4c23fe9d256d1557faeef057d0987cda3caf
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-03-17 23:13:17 -0400

    fix suppliers

--- backend/app/Http/Controllers/ProxyController.php
+++ backend/app/Http/Controllers/ProxyController.php
@@ -25,21 +25,22 @@ class ProxyController extends Controller
     }
 
     public function getDocument(Request $request) {
+
         $client = new \GuzzleHttp\Client();
         $url = env('APP_URL').'/storage/'.strtolower($request->file);
-    
-        $response = $client->get($url);
-    
+
+        $response = $client->get($url, [
+            'verify' => false
+        ]);
+
         if ($response->getStatusCode() == 200) {
-            $headers = [
-                'Content-Type' => $response->getHeaderLine('Content-Type'),
-                'Content-Length' => $response->getHeaderLine('Content-Length'),
-                'Content-Disposition' => 'attachment; filename="' . basename($request->file) . '"'
-            ];
-    
-            return response()->stream(function () use ($response) {
-                echo $response->getBody();
-            }, 200, $headers);
+            $fileContents = $response->getBody()->getContents();
+            $fileSize = strlen($fileContents); 
+
+            $tempPath = 'temp/' . $request->file;
+            Storage::disk('local')->put($tempPath, $fileContents);
+
+            return response()->download(storage_path('app/' . $tempPath))->deleteFileAfterSend(true);
         } else {
             return response()->json(['error' => 'Unable to download the file'], 500);
         }
