c3f13eb1101e817152b5acc06a421f923eadb1bb
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-04-12 04:11:57 -0400

    fix products

--- backend/app/Http/Controllers/MiscellaneousController.php
+++ backend/app/Http/Controllers/MiscellaneousController.php
@@ -82,7 +82,7 @@ class MiscellaneousController extends Controller
             $products = Product::with(['user'])->get();
 
             $query = Product::with(['user', 'order'])
-                            ->distinct('products.name')
+                            ->distinct()
                             ->join('product_lists as pl', 'pl.product_id', 'products.id')
                             ->where('state_id', 3)
                             ->applyFilters(
