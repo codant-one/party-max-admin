cf158dd66b675ab5bae1744771831b4a9613e100
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-05-23 16:56:23 -0400

    fix emails

--- backend/resources/views/emails/payment/purchase_detail.blade.php
+++ backend/resources/views/emails/payment/purchase_detail.blade.php
@@ -16,20 +16,24 @@
                 <tr>
                     <td>
                         <table width="100%" style="padding-left: 40px; margin-bottom: 5px;">
+                            <tr>
                             <td style="width: 120px; height: 120px; max-width: 120px; border-radius: 16px; border: 1px solid #E2F8FC; text-align: center;">
                                 <img src="{{ $product['product_image'] }}" width="90%">
                             </td>
-                            <td>
-                                <span style="font-size: 24px; color: #0a1b33; margin-left: 40px; margin-botton: 2px !important;">
-                                    {{ $product['product_name'] }}
-                                </span><br>
-                                <span style="font-size: 15px; color: #999999; margin-left: 40px;">
-                                    Color: {{ $product['color'] }}
-                                </span><br>
-                                <span style="font-size: 15px; color: #999999; margin-left: 40px;">
-                                    {{ $product['quantity'] }} {{ $product['text_quantity']}}
-                                </span>
+                            <td style="justify-content: start; align-items: center; display: flex; height: 120px;">
+                                <div style="display:block;">
+                                    <span style="display: block; font-size: 24px; color: #0a1b33; margin-left: 40px;">
+                                        {{ $product['product_name'] }}
+                                    </span>
+                                    <span style="display: block; font-size: 15px; color: #999999; margin-left: 40px;">
+                                        Color: {{ $product['color'] }}
+                                    </span>
+                                    <span style="display: block; font-size: 15px; color: #999999; margin-left: 40px;">
+                                        {{ $product['quantity'] }} {{ $product['text_quantity']}}
+                                    </span>
+                                </div>
                             </td>
+                        </tr>
                         </table>
                     </td>     
                 </tr>
@@ -43,12 +47,7 @@
                     <td>
                         <table width="100%" style="padding-left: 40px;">
                             <td width="10%" style="vertical-align: top;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0 3C0 2.44772 0.447715 2 1 2H16C16.5523 2 17 2.44772 17 3V16C17 16.5523 16.5523 17 16 17H1C0.447715 17 0 16.5523 0 16V3ZM2 4V15H15V4H2Z" fill="#0A1B33"/>
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M15 8C15 7.44772 15.4477 7 16 7H20C20.2652 7 20.5196 7.10536 20.7071 7.29289L23.7071 10.2929C23.8946 10.4804 24 10.7348 24 11V16C24 16.5523 23.5523 17 23 17H16C15.4477 17 15 16.5523 15 16V8ZM17 9V15H22V11.4142L19.5858 9H17Z" fill="#0A1B33"/>
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M5.5 17C4.67157 17 4 17.6716 4 18.5C4 19.3284 4.67157 20 5.5 20C6.32843 20 7 19.3284 7 18.5C7 17.6716 6.32843 17 5.5 17ZM2 18.5C2 16.567 3.567 15 5.5 15C7.433 15 9 16.567 9 18.5C9 20.433 7.433 22 5.5 22C3.567 22 2 20.433 2 18.5Z" fill="#0A1B33"/>
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M18.5 17C17.6716 17 17 17.6716 17 18.5C17 19.3284 17.6716 20 18.5 20C19.3284 20 20 19.3284 20 18.5C20 17.6716 19.3284 17 18.5 17ZM15 18.5C15 16.567 16.567 15 18.5 15C20.433 15 22 16.567 22 18.5C22 20.433 20.433 22 18.5 22C16.567 22 15 20.433 15 18.5Z" fill="#0A1B33"/>
-                                </svg>
+                                <img src="{{ asset('/images/truck.png') }}" alt="Facebook" title="Facebook" width="24" height="24" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic">
                             </td>
                             <td width="90%">
                                 <p style="font-size: 16px; color: #0a1b33; font-weight: 700; margin: 0;">Envío a domicilio</p>
@@ -64,10 +63,7 @@
                     <td>
                         <table width="100%" style="padding-left: 40px; margin-top: 16px;">
                             <td width="10%" style="vertical-align: top;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C12.5523 0 13 0.447715 13 1V23C13 23.5523 12.5523 24 12 24C11.4477 24 11 23.5523 11 23V1C11 0.447715 11.4477 0 12 0Z" fill="#0A1B33"/>
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M6.31802 5.31802C7.16193 4.47411 8.30653 4 9.5 4H17C17.5523 4 18 4.44772 18 5C18 5.55228 17.5523 6 17 6H9.5C8.83696 6 8.20107 6.26339 7.73223 6.73223C7.26339 7.20107 7 7.83696 7 8.5C7 9.16304 7.26339 9.79893 7.73223 10.2678C8.20107 10.7366 8.83696 11 9.5 11H14.5C15.6935 11 16.8381 11.4741 17.682 12.318C18.5259 13.1619 19 14.3065 19 15.5C19 16.6935 18.5259 17.8381 17.682 18.682C16.8381 19.5259 15.6935 20 14.5 20H6C5.44772 20 5 19.5523 5 19C5 18.4477 5.44772 18 6 18H14.5C15.163 18 15.7989 17.7366 16.2678 17.2678C16.7366 16.7989 17 16.163 17 15.5C17 14.837 16.7366 14.2011 16.2678 13.7322C15.7989 13.2634 15.163 13 14.5 13H9.5C8.30653 13 7.16193 12.5259 6.31802 11.682C5.47411 10.8381 5 9.69347 5 8.5C5 7.30653 5.47411 6.16193 6.31802 5.31802Z" fill="#0A1B33"/>
-                                </svg>
+                                <img src="{{ asset('/images/dollar-sign.png') }}" alt="Facebook" title="Facebook" width="24" height="24" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic">
                             </td>
                             <td width="90%">
                                 <p style="font-size: 16px; color: #0a1b33; font-weight: 700; margin: 0;">Pagaste ${{ $data['total'] }}</p>
