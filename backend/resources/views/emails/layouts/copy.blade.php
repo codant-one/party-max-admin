01e83bb6fce4837e69dacf59ff57ed92a89735e5
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-05-29 23:06:11 -0400

    fix terms and conditions

--- backend/resources/views/emails/layouts/copy.blade.php
+++ backend/resources/views/emails/layouts/copy.blade.php
@@ -3,13 +3,26 @@
         <td align="center" style="padding:0;Margin:0">
             <table class="es-content-body" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;width:600px">
                 <tr>
-                    <td align="left" bgcolor="#E2F8FC" style="Margin:0;padding-top:25px;padding-bottom:25px;padding-left:40px;padding-right:40px;background-color:#e2f8fc">
+                    <td align="left" bgcolor="#E2F8FC" style="Margin:0;padding-top:25px;padding-bottom:25px;padding-left:40px;padding-right:40px;background-color:#e2f8fc;border-top: 1px solid #D9EEF2;">
                         <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                             <tr>
                                 <td valign="top" align="center" style="padding:0;Margin:0;width:520px">
                                     <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                                         <tr>
-                                            <td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#0a1b33;font-size:14px">Nunca envíes tu clave o datos de tu cuenta por e-mail.<br>Conoce cómo cuidamos tu Privacidad y visita los Términos y Condiciones.</p></td>
+                                            <td align="center" style="padding:0;Margin:0">
+                                                <p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#0a1b33;font-size:14px">
+                                                    Nunca envíes tu clave o datos de tu cuenta por e-mail.<br>
+                                                    Conoce cómo cuidamos tu 
+                                                    <a href="{{ env('APP_DOMAIN') }}/privacy-statement" target="_blank" style="color: #0a1b33!important;">
+                                                        Privacidad 
+                                                    </a>
+                                                    y visita los 
+                                                    <a href="{{ env('APP_DOMAIN') }}/terms-and-conditions" target="_blank" style="color: #0a1b33!important;">
+                                                        Términos y Condiciones. 
+                                                    </a>
+                                                    
+                                                </p>
+                                            </td>
                                         </tr>
                                     </table>
                                 </td>
