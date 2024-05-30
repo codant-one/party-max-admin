38bdd02dcab636c078d1d4f6e99d4de0f1b63854
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-05-29 23:09:49 -0400

    fix terms and conditions

--- backend/resources/views/emails/layouts/copy.blade.php
+++ backend/resources/views/emails/layouts/copy.blade.php
@@ -13,14 +13,9 @@
                                                 <p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#0a1b33;font-size:14px">
                                                     Nunca envíes tu clave o datos de tu cuenta por e-mail.<br>
                                                     Conoce cómo cuidamos tu 
-                                                    <a href="{{ env('APP_DOMAIN') }}/privacy-statement" target="_blank" style="color: #0a1b33!important;">
-                                                        Privacidad 
-                                                    </a>
+                                                    <a href="{{ env('APP_DOMAIN') }}/privacy-statement" target="_blank" style="color: #0a1b33!important;">Privacidad</a>
                                                     y visita los 
-                                                    <a href="{{ env('APP_DOMAIN') }}/terms-and-conditions" target="_blank" style="color: #0a1b33!important;">
-                                                        Términos y Condiciones. 
-                                                    </a>
-                                                    
+                                                    <a href="{{ env('APP_DOMAIN') }}/terms-and-conditions" target="_blank" style="color: #0a1b33!important;">Términos y Condiciones</a>.                                                   
                                                 </p>
                                             </td>
                                         </tr>
