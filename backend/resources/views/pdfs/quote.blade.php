6a376c1b4004313155a7dd4fa6fec0b85eb97194
SteffaniCastro <steffani.castro.useche@gmail.com>
2025-04-06 19:57:58 -0400

    fix quotes

--- backend/resources/views/pdfs/quote.blade.php
+++ backend/resources/views/pdfs/quote.blade.php
@@ -83,46 +83,55 @@
         margin: 0 !important;
       }
 
-      .border-title {
+      .border-bottom {
         border-bottom: 1px solid #D9EEF2;
       }
+
+      .border-top {
+        border-top: 1px solid #D9EEF2;
+      }
+
+      .text-primary {
+        color: #FF0090;
+      }
+
   </style>
   </head>
   <body style="width:100%;font-family:arial, 'helvetica neue', helvetica, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;margin:0">
       <header></header>
       <footer></footer>
       <div class="content">
-        <table cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top">
+        <table cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top;">
           <tr>
-            <td>CLIENTE</td>
-            <td style="text-align: right">COTIZACIÓN # {{$quote->id}}</td>
+            <td style="font-weight: 700; font-size: 16px;">CLIENTE</td>
+            <td style="text-align: right; font-weight: 700; font-size: 16px;">COTIZACIÓN # {{$quote->id}}</td>
           </tr>
-          <tr class="border-title">
+          <tr>
             <td>
-                <p class="m-0">Nombre: {{ $quote->name }}</p>
+                <p class="m-0"><strong>Nombre:</strong> {{ $quote->name }}</p>
                 @if($quote->company)
-                <p class="m-0">Empresa: {{ $quote->company }}</p>
+                <p class="m-0"><strong>Empresa:</strong>  {{ $quote->company }}</p>
                 @endif
-                <p class="m-0">Documento: ({{ $quote->document_type->code }}) - {{ $quote->document }}</p>
-                <p class="m-0">Teléfono: {{substr($quote->phone, 0, 3) === '+57' ? '' : '(+57)'}} {{ $quote->phone }}</p>
-                <p class="m-0">Correo electrónico: {{ $quote->email }}</p>
+                <p class="m-0"><strong>Documento:</strong>  ({{ $quote->document_type->code }}) - {{ $quote->document }}</p>
+                <p class="m-0"><strong>Teléfono:</strong>  {{substr($quote->phone, 0, 3) === '+57' ? '' : '(+57)'}} {{ $quote->phone }}</p>
+                <p class="m-0"><strong>Correo electrónico:</strong>  {{ $quote->email }}</p>
             </td>
             <td style="text-align: right; vertical-align: top;">
-                <p class="m-0">Fecha de solicitud: {{$quote->start_date}}</p>
-                <p class="m-0">Fecha de vencimiento: {{$quote->due_date}}</p>
+                <p class="m-0"><strong>Fecha de solicitud:</strong>  {{$quote->start_date}}</p>
+                <p class="m-0"><strong>Fecha de vencimiento:</strong>  {{$quote->due_date}}</p>
             </td>
           </tr>
         </table>
-        <table cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top">
-          <tr class="border-title">
-            <td class="products_img">Productos</td>
+        <table cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top; margin-top: 20px;">
+          <tr class="border-bottom border-top">
+            <td class="products_img"><strong>Productos</strong></td>
             <td class="products"></td>
-            <td class="products_cant">Cant.</td>
-            <td class="products_cant">Unitario</td>
-            <td class="products_cant">Total</td>
+            <td class="products_cant"><strong>Cant.</strong></td>
+            <td class="products_cant"><strong>Unitario</strong></td>
+            <td class="products_cant"><strong>Total</strong></td>
           </tr>
           @foreach($products as $product)
-          <tr class="border-title">
+          <tr class="border-bottom">
               <td class="products_img">
                 <a href="{{$product['slug']}}" class="es-button" target="_blank" style="width: 60px; min-height: 50px; border-radius: 8px; border: 1px solid #E2F8FC; text-align: center; align-items: center; justify-content: center; display: flex !important;">
                       <img src="{{ $product['product_image'] }}" width="90%">
@@ -152,7 +161,7 @@
           </tr>
           @endforeach
           @foreach($services as $service)
-          <tr class="border-title">
+          <tr class="border-bottom">
             <td class="products_img">
               <a href="{{$service['slug']}}" class="es-button" target="_blank" style="width: 60px; min-height: 50px; border-radius: 8px; border: 1px solid #E2F8FC; text-align: center; align-items: center; justify-content: center; display: flex !important;">
                   <img src="{{ $service['service_image'] }}" width="90%">
@@ -195,29 +204,34 @@
               </td>  
           </tr>
           @endforeach
-          <tr class="border-title">
+          <tr class="border-bottom">
             <td class="products_img"></td>
             <td class="products"></td>
             <td class="products_cant"></td>
             <td class="products_cant">
-              <p class="text m-0">Subtotal:</p>
-              <p class="text m-0">Tax:</p>
-              <p class="text m-0">Total:</p>
+              <p class="text m-0"><strong>Subtotal:</strong></p>
+              <p class="text m-0"><strong>Tax:</strong></p>
+              <p class="text m-0 text-primary"><strong>Total:</strong></p>
             </td>
             <td class="products_cant" style="padding: 10px 0;">
-              <p class="numbers m-0"><span>${{ formatCurrency($quote->sub_total) }}</span></p>
-              <p class="numbers m-0"><span>${{ formatCurrency($quote->tax) }}</span></p>
-              <p class="numbers m-0"><span>${{ formatCurrency($quote->total) }}</span></p>
+              <p class="numbers m-0"><strong>${{ formatCurrency($quote->sub_total) }}</strong></p>
+              <p class="numbers m-0"><strong>${{ formatCurrency($quote->tax) }}</strong></p>
+              <p class="numbers m-0 text-primary"><strong>${{ formatCurrency($quote->total) }}</strong></p>
             </td>
           </tr>
           <tr>
             <td colspan="5" style="padding: 10px 0;">
-              <span class="ms-2">
-                Los precios mostrados en esta cotización corresponden únicamente a los productos seleccionados. <br>
-                El costo de envío no está incluido y se calculará por separado, según la dirección de entrega. <br>
-                Todos nuestros productos en el marketplace incluyen el IVA. <br>
-                Esta cotización tiene una validez de 8 días a partir de la fecha de emisión.
-              </span>
+              <div style="display: flex; align-items: center;">
+                <span>
+                  <img src="{{ asset('/images/info-circle.svg') }}" width="25" alt="info">  
+                </span>
+                <span style="margin-left: 10px;">
+                  Los precios mostrados en esta cotización corresponden únicamente a los productos seleccionados. <br>
+                  El costo de envío no está incluido y se calculará por separado, según la dirección de entrega. <br>
+                  Todos nuestros productos en el marketplace incluyen el IVA. <br>
+                  Esta cotización tiene una validez de 8 días a partir de la fecha de emisión.
+                </span>
+              </div>
             </td>
           </tr>
         </table>
