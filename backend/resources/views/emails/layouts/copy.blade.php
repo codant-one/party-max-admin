f89e97f7c5f8d96354bdf207e31c15f3422f51e0
SteffaniCastro <steffani.castro.useche@gmail.com>
2025-05-09 17:00:34 -0400

    figit push

--- backend/resources/views/emails/layouts/copy.blade.php
+++ backend/resources/views/emails/layouts/copy.blade.php
@@ -13,9 +13,12 @@
                                                 <p style="margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:17px;color:#0a1b33;font-size:14px">
                                                     Nunca envíes tu clave o datos de tu cuenta por e-mail.<br>
                                                     Conoce cómo cuidamos tu 
-                                                    <a href="{{ env('APP_DOMAIN') }}/privacy-statement" target="_blank" style="color: #0a1b33!important;">Privacidad</a>
+                                                    <a href="{{ env('APP_DOMAIN') }}/privacy-statement" target="_blank" style="color: #0a1b33!important;">privacidad</a>
                                                     y visita los 
-                                                    <a href="{{ env('APP_DOMAIN') }}/terms-and-conditions" target="_blank" style="color: #0a1b33!important;">Términos y Condiciones</a>.                                                   
+                                                    <a href="{{ env('APP_DOMAIN') }}/terms-and-conditions" target="_blank" style="color: #0a1b33!important;">términos y condiciones</a>                                                   
+                                                    , así como nuestra 
+                                                    <a href="{{ env('APP_DOMAIN') }}/data-protection" target="_blank" style="color: #0a1b33!important;">política de protección de datos</a>.
+                                                    
                                                 </p>
                                             </td>
                                         </tr>
